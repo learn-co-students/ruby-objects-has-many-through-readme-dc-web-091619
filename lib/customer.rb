2e5933f79b8fa1ab1822ad275245e48d2a1240be
Nick <nboober@terpmail.umd.edu>
2019-09-19 17:17:53 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,35 @@
 class Customer
-  
+
+    attr_accessor :name, :age
+
+  @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def meals
+    Meal.all.select do |meal|
+      meal.customer == self
+    end
+  end
+
 end
+
+# sam = Customer.new("Sam", 27)
+
+# sam.new_meal(pat, 50, 10) 
+# # A Customer creates a Meal, passing in a Waiter instance
+
+# sam.new_meal(alex, 20, 3) 
+# A Customer creates a Meal, passing in a Waiter instance
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,27 @@
 class Waiter
 
+    attr_accessor :name, :yrs_experience
+
+    @@all = []
+  
+    def initialize(name, yrs_experience)
+      @name = name
+      @yrs_experience = yrs_experience
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+
+    def new_meal(customer, total, tip=0)
+        Meal.new(self, customer, total, tip)
+    end
+    
 end
+
+# pat = Waiter.new("Pat", 2)
+# alex = Waiter.new("Alex", 5)
+
+# pat.new_meal(sam, 30, 5) 
+# A Waiter creates a Meal, passing in a Customer instance
