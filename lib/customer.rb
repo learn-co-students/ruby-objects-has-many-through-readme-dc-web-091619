c87daa4c4d581a9ecd1d68add6a0bd99d7ab8c09
Graham Flaspoehler <grahamflas@gmail.com>
2019-09-22 21:50:00 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,41 @@
+require "pry"
 class Customer
+  attr_accessor :name, :age
+
+  ###====== CLASS ======
+  @@all = []
   
+  def self.all
+    @@all
+  end
+  
+  ###====== INSTANCE ======
+  def initialize(name, age)
+    @name = name
+    @age = age
+    save
+  end
+
+  def save
+    Customer.all << self
+  end
+
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def meals 
+    Meal.all.select do |meal|
+      meal.customer == self
+    end
+  end
+ 
+  def waiters 
+    meals.map do |meal|
+      meal.waiter
+    end
+  end
+
+
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,43 @@
+require "pry"
 class Waiter
+  attr_accessor :name, :yrs_experience
+
+    ###====== CLASS ======
+    @@all = []
+  
+    def self.all
+      @@all
+    end
+    
+    ###====== INSTANCE ======
+    def initialize(name, yrs_experience)
+      @name = name
+      @yrs_experience = yrs_experience
+      save
+    end
+
+    def save 
+      Waiter.all << self
+    end
+
+    def new_meal(customer, total, tip=0)
+      Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+      Meal.all.select do |meal|
+        meal.waiter == self
+      end
+    end
+
+    def best_tipper
+     
+      best_tipped_meal = meals.max do |meal_a, meal_b|
+        meal_a.tip <=> meal_b.tip
+      end
+
+      best_tipped_meal.customer
+    end
+
 
 end
