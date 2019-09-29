1561bfa558838c8b2afcf897a1773bd49fc87f50
saltydog79 <aspentree79@gmail.com>
2019-09-29 10:39:03 -0400

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,27 @@
 class Meal
+    @@all = []
+
+    attr_accessor :waiter, :customer, :total, :tip
+
+    def initialize(waiter, customer, total, tip=0)
+        @waiter = waiter
+        @customer = customer
+        @total = total
+        @tip = tip
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 
 end
+
+
+
+
+# m1 = Meal.new("Pete", "Joe", 35, 10)
+# m2 = Meal.new("Jack", "Patty", 30, 5)
+# m3 = Meal.new("Sara", "Mike", 25, 5)
+# m4 = Meal.new("Samuel", "Mike", 55, 20)
+# m5 = Meal.new("Charlene", "Charlie", 60, 10)
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,43 @@
 class Waiter
+    @@all = []
+    attr_reader :name, :yrs_experience
+  
+  def initialize(name, yrs_experience)
+  @name = name
+  @yrs_experience = yrs_experience
+  @@all << self
+  end
+  
+  def self.all
+  @@all
+  end
+
+
+  def new_meal(customer, total, tip=0)
+      Meal.new(self, customer, total, tip)
+    #   NEW MEAL INSTANCE FOR A WAITER
+    # same as in customer.rb but self here is waiter
+  end
+
+  def meals
+    Meal.all.select do |meal|
+        meal.waiter == self
+        end 
+    # iterates through each meal instance and extracts only those that match (==) this waiter instance
+    end
+
+    def best_tipper
+        best_tipper = meals.max do |meal_1, meal_2|
+            meal_1.tip <=> meal_2.tip
+        end
+        best_tipper.customer
+    end
 
 end
+
+
+# pete = Waiter.new("Pete", 10)
+# jack = Waiter.new("Jack", 2)
+# sara = Waiter.new("Sara", 5)
+# samuel = Waiter.new("Samuel", 5)
+# charlene = Waiter.new("Charlene", 15)
